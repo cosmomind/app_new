ef5d13c49736154c4a520680808d8ac8ea1d7124
cosmomind <riya.mathew2020@vitstudent.ac.in>
2021-04-17 23:34:52 +0530

    3 pages done

--- app/src/main/java/com/example/first_task/MainActivity.java
+++ app/src/main/java/com/example/first_task/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.first_task;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText button =(EditText)findViewById(R.id.button);
+        EditText button1 =(EditText)findViewById(R.id.button1);
+        button10 = (Button) findViewById(R.id.button10);
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,signup.class);
+                intent.putExtra("data",button.getText().toString());
+                intent.putExtra("data1",button1.getText().toString());
+
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
+
+
 }
--- app/src/main/java/com/example/first_task/signup.java
+++ app/src/main/java/com/example/first_task/signup.java
@@ -1,31 +1,46 @@
 package com.example.first_task;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class signup extends AppCompatActivity {
 
-    Button button4;
+    Button button6;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        button4 = findViewById(R.id.button4);
-        button4.setOnClickListener(new View.OnClickListener() {
+
+        TextView button3 =(TextView)findViewById(R.id.button3);
+        Bundle bundle = getIntent().getExtras();
+        String data = bundle.getString("data");
+        button3.setText(data);
+
+        TextView button4 = (TextView)findViewById(R.id.button4);
+        Bundle bundle1 = getIntent().getExtras();
+        String data1 = bundle1.getString("data1");
+        button4.setText(data1);
+
+
+        button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(signup.this, signup2.class);
-
-
-                startActivity(intent);
+                Intent in = new Intent(signup.this, signup2.class);
+                startActivity(in);
 
             }
         });
+
+
+
 
     }
 }
